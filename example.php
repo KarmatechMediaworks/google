f5a3583a88f23c25c9d68f7bc540e4d5dec47a29
Karmatech Mediaworks <rahulkhosla@Karmatech.local>
2017-05-05 12:23:06 +0530

    Recommit - 5-may

--- example.php
+++ example.php
@@ -1,78 +1,56 @@
-<?php
-// use ElephantIO\Client,
-//     ElephantIO\Engine\SocketIO\Version0X;
+<?php header('Access-Control-Allow-Origin: *'); ?>
 
-// require __DIR__ . '/vendor/autoload.php';
-// use Ytake\WebSocket\Io;
+<?php 
+
+require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
+
+//$phpCricket = new PhpCricket\PhpCriclib('your_access_key', 'your_secret_key', 'your_app_id', 'unique_device_id');
+
+$phpCricket = new PhpCricket\PhpCriclib('9bfab442448c046a069b74525f6bf8bd', '451a569e27adffba4ed1b973107333fc', 'motivatorworld.com', '14920807632061');
 
 
-require_once 'lzconfig.php';
-require_once 'lz.php';
+/**
+* NOTE: To access the Cricket API's data, you need Valid Match Keys.
+*
+* Here, you may try with some Free Match Keys.
+*
+*/
+
+/* Get Match Details */
+ //$getMatch = $phpCricket->getMatch('iplt20_2017_g38', 'summary_card');
+ //echo json_encode($getMatch);
 
 
-$authData = auth();
-if (! $authData) {
-	echo "Error Auth, Please verify your access details on lzconfig.php \n";
-	return;
-}
+/* Get BallbyBall Details */
+/*$getBallbyBall = $phpCricket->getBallByBall('dev_season_2014_q3', 'b_1_10');
+ echo json_encode($getBallbyBall);*/
 
-$authData;
-$token = $authData['access_token'];
 
-/* get Match API data. Allowed card types are micro_card, summary_card and full_card. For more info https://developers.litzscore.com/docs/match_api/ */
-//print_r(getMatch($token, 'iplt20_2013_g30', 'micro_card')); 
+/* Get Recent Match Details */
+	//$getRecentMatch = $phpCricket->getRecentMatch('iplt20_2017', 'micro_card');
+	
 
+	
+	
 
 try {
 
-    date_default_timezone_set('Asia/kolkata'); 
-
-    $matchkey1="iplt20_2017_g38";
-    $matchkey2="iplt20_2017_g43";
-    $matchkey3="iplt20_2017_g46";
-    $matchkey4="iplt20_2017_g56";
-
-    $date1="";  //default
-    $date2="";  //5
-    $date3="";  //7
-    $date4="";  //14
-
-
-    $matchkey=$matchkey1;
+    $matchkey="iplt20_2017";
 
     if($matchkey!="")
     {
 
-          //$getMatch = $phpCricket->getMatch($matchkey, 'summary_card');
-
-          $getMatch=getMatch($token, $matchkey, 'summary_card');
+		  	$getRecentMatch = $phpCricket->getRecentMatch('iplt20_2017', 'summary_card');
+	
+	    	echo $jsondata=json_encode($getRecentMatch);
           
-         echo  $jsondata=json_encode($getMatch);
-
-          $decodejsondata=json_decode($jsondata, true);
-
-          //var_dump($decodejsondata);
-
-          $result=$decodejsondata['card']['toss'];
-
-          if(empty($result))
-          {
-
-              //Array is empty or match ID is wrong. Please check.
-
-          }else
-          { 
-
-            //var_dump($jsondata);
+			$decodejsondata=json_decode($jsondata, true);
 
             //Insert the json response into the response.json
 
               $fp = fopen('response.json', 'w');
               fwrite($fp, json_encode($decodejsondata));
               fclose($fp);
-
-            
-           }      
 
     }
 }
@@ -81,46 +59,59 @@ catch(Exception $e) {
    echo 'some error occured in the Api call: ' .$e->getMessage();
 
 }
+	
 
 
+/* Get Recent Season Details */
+// $getRecentSeason = $phpCricket->getRecentSeason();
+// echo json_encode($getRecentSeason);
 
 
-
-/* get RecentMatch API data. Allowed card types are micro_card and summary_card. For more info https://developers.litzscore.com/docs/recent_match_api/ */
-// print_r(getRecentMatch($token, 'micro_card'));  
-
-/* get RecentSeasonMatch API data. Allowed card types are micro_card and summary_card. For more info https://developers.litzscore.com/docs/recent_match_api/ */
-// print_r(getRecentSeasonMatch($token, 'dev_season_2014',  'micro_card')); 
-
-/* get RecentSeason API data. For more info https://developers.litzscore.com/docs/recent_season_api/ */
-// print_r(getRecentSeason($token));
-
-/* get Schedule API data. Allowed formates are YYYY-MM-DD and YYYY-MM. For more info https://developers.litzscore.com/docs/schedule_api/ */
-// print_r(getSchedule($token, 'YYYY-MM-DD')); 
-
-/* get Season Schedule API data. Allowed formates are YYYY-MM-DD and YYYY-MM.  For more info https://developers.litzscore.com/docs/schedule_api/ */
-// print_r(getSeasonSchedule($token, 'dev_season_2014', 'YYYY-MM'));
-
-/* get Season API data. Allowed card type micro_card and summary_card. For more info https://developers.litzscore.com/docs/season_api/ */
-// print_r(getSeason($token, 'dev_season_2014', 'micro_card' ));
+/* Get Schedule Details */
+// $getSchedule = $phpCricket->getSchedule('2013-05');
+// echo json_encode($getSchedule);
 
 
-/* get Season Stats API data. For more info https://developers.litzscore.com/docs/season_stats_api/ */
-// print_r(getSeasonStats($token, 'dev_season_2014'));
-
-/* get Season Points API data. For more info https://developers.litzscore.com/docs/season_points_api/ */
-// print_r(getSeasonPoints($token, 'dev_season_2014'));
-
-/* get Season Player Stats API data. player_x1 is player key. For more info https://developers.litzscore.com/docs/season_player_stats_api/ */
-// print_r(getSeasonPlayerStats($token, 'dev_season_2014', 'player_x1'));
-
-/* get Match Over Summary API data. For more info https://developers.litzscore.com/docs/over_summary_api/ */
-// print_r(getMatchSummary($token, 'iplt20_2013_g30'));
-
-/* get News Aaggregation API data.  For more info https://developers.litzscore.com/docs/news_aggregation_api/ */
-// print_r(getNewsAaggregation($token));
+/* Get Season Schedule Details */
+// $getSeasonSchedule = $phpCricket->getSeasonSchedule('dev_season_2014', '2013-05');
+// echo json_encode($getSeasonSchedule);
 
 
-/* get Ball by Ball API data. b_1_1 is over key. For more info https://developers.litzscore.com/docs/ball_by_ball_api/ */
-// print_r(getBallByBall($token, 'iplt20_2013_g30', 'b_1_1'));
+/* Get Player Stats Details */
+// $getPlayerStats = $phpCricket->getPlayerStats('ms_dhoni', 'icc');
+// echo  json_encode($getPlayerStats);
 
+
+/* Get Season Player Stats Details */
+// $getSeasonPlayerStats = $phpCricket->getSeasonPlayerStats('asiacup_2016', 'ms_dhoni');
+// echo json_encode($getSeasonPlayerStats);
+
+
+/* Get Season Details */
+// $getSeason = $phpCricket->getSeason('dev_season_2014', 'micro_card');
+// echo json_encode($getSeason);
+
+
+/* Get Season Stats Details */
+// $getSeasonStats = $phpCricket->getSeasonStats('dev_season_2014');
+// echo json_encode($getSeasonStats);
+
+
+/* Get Season Points Details */
+// $getSeasonPoints = $phpCricket->getSeasonPoints('dev_season_2014');
+// echo json_encode($getSeasonPoints);
+
+
+/* Get Season Team Details */
+// $getSeasonTeam = $phpCricket->getSeasonTeam('dev_season_2014', 'dev_season_2014_teamx', 'icc');
+// echo json_encode($getSeasonTeam);
+
+
+/* Get Overs Summary Details */
+// $getOverSummary = $phpCricket->getOversSummary('dev_season_2014_q6');
+// echo json_encode($getOverSummary);
+
+
+/* Get News Aggregation */
+// $getNewsAgg = $phpCricket->getNewsAggregation();
+// echo json_encode($getNewsAgg);
--- lzconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('LZ_url', 'https://rest.cricketapi.com/rest/v2/');
-define('LZ_access_key', '9bfab442448c046a069b74525f6bf8bd');
-define('LZ_secret_key', '451a569e27adffba4ed1b973107333fc');
-define('LZ_app_id', 'motivatorworld.com');
-define('LZ_device_id', '14920807632061');
